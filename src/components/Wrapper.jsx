8e881d849374e6d34d0f4ec9ef288b517c0b2097
vickyDevlo <vickymagare1996@gmail.com>
2025-02-05 17:02:26 +0530

    changes done.

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -4,7 +4,7 @@ export const Wrapper = ({ children, className }) => {
   
   return (
     <div className="container mx-auto h-screen flex items-center justify-center px-2">
-      <div className={`flex flex-col bg-white w-[285px] md:w-[500px] rounded-lg shadow-2xl items-center justify-center gap-3 py-8 px-3 ${className}`}>
+      <div className={`flex flex-col bg-white w-[300px] md:w-[500px] rounded-lg shadow-2xl items-center justify-center gap-3 py-8 px-3 ${className}`}>
         {children}
       </div>
     </div>
